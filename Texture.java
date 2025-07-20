59536379c7044b93159c8519f3c11e7c8511822f
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-20 16:22:49 +0530

    initializing variables

--- Texture.java
+++ Texture.java
@@ -3,3 +3,9 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+public class Texture {
+	public int[] pixels;
+	private String loc;
+	public final int SIZE;
+
+    
