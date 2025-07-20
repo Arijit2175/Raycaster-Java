ab76a09924cd1afe2e3cb819012a733dfa0497dd
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-20 16:23:31 +0530

    for loading the textures

--- Texture.java
+++ Texture.java
@@ -15,4 +15,15 @@ public class Texture {
 		load();
 	}
 
+    private void load() {
+		try {
+			BufferedImage image = ImageIO.read(new File(loc));
+			int w = image.getWidth();
+			int h = image.getHeight();
+			image.getRGB(0, 0, w, h, pixels, 0, w);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
     
