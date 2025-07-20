dc727488e18ad97a003e386498139e6f13bb9fd2
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-07-20 16:29:35 +0530

    initializing screen variables

--- Screen.java
+++ Screen.java
@@ -1,3 +1,18 @@
 import java.util.ArrayList;
 import java.awt.Color;
 
+public class Screen {
+	public int[][] map;
+	public int mapWidth, mapHeight, width, height;
+	public ArrayList<Texture> textures;
+	
+	public Screen(int[][] m, int mapW, int mapH, ArrayList<Texture> tex, int w, int h) {
+		map = m;
+		mapWidth = mapW;
+		mapHeight = mapH;
+		textures = tex;
+		width = w;
+		height = h;
+	}
+
+    
